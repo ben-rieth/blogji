e9e5c4558735bd781c0b682a22f9dd890fd211fd
ben-rieth <benrieth3@gmail.com>
2022-12-10 20:37:58 -0600

    Adjsut size of post letters as size gets bigger

--- src/components/organisms/PostGrid.tsx
+++ src/components/organisms/PostGrid.tsx
@@ -11,7 +11,7 @@ const PostGrid:FC<PostGridProps> = ({ posts, title }) => {
     return (
         <div className="my-3">
             <h2 className="font-bold text-3xl text-white ml-5">{title}</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4">
                 {posts.map((post) => (
                     <PostCover key={post.id} post={post}/>
                 ))}
