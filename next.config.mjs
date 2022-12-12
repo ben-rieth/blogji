d3445010de7324eb408d8271c7526ee9e382cee7
ben-rieth <benrieth3@gmail.com>
2022-12-12 17:44:44 -0600

    Add security headers

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+`;
+
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
@@ -13,5 +17,30 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ]
+      }
+    ]
+  }
 };
 export default config;
