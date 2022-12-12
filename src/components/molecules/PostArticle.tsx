df348673220f57400695127bf89f67c8d540b348
ben-rieth <benrieth3@gmail.com>
2022-12-12 16:33:24 -0600

    Limit how wide a post can get

--- src/components/molecules/PostArticle.tsx
+++ src/components/molecules/PostArticle.tsx
@@ -9,7 +9,7 @@ const PostArticle:FC<PostArticleProps> = ({ articleCode }) => {
     const Component = useMemo(() => getMDXComponent(articleCode), [articleCode])
     
     return (
-        <article className="text-black dark:text-white mx-10 mt-10 text-lg">
+        <article className="text-black dark:text-white px-10 mx-auto mt-10 text-lg max-w-screen-lg">
             <Component components={{ 
                 ul: (props) => <ul className="list-disc ml-10" {...props} />
             }}/>
--- src/components/molecules/PostHeader.tsx
+++ src/components/molecules/PostHeader.tsx
@@ -8,12 +8,14 @@ type PostHeaderProps = {
 
 const PostHeader:FC<PostHeaderProps> = ({ title, category }) => {
     return (
-        <header className="h-96 dark:bg-slate-700 bg-slate-300 pt-48 px-10">
-            <Breadcrumbs>
-                <BreadcrumbsLink title="Home" href="/"/>
-                <BreadcrumbsLink title={category} />
-            </Breadcrumbs>
-            <h1 className="text-slate-800 dark:text-white text-3xl sm:text-4xl font-semibold">{title}</h1>
+        <header className="h-80 dark:bg-slate-700 bg-slate-300 pt-44 pb-12">
+            <div className="mx-auto max-w-screen-lg px-10">
+                <Breadcrumbs>
+                    <BreadcrumbsLink title="Home" href="/"/>
+                    <BreadcrumbsLink title={category} />
+                </Breadcrumbs>
+                <h1 className="text-slate-800 dark:text-white text-3xl sm:text-4xl font-semibold mt-3">{title}</h1>
+            </div>
         </header>
     )
     
