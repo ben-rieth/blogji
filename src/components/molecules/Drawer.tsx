6f746fb529a02933e9c2bc876a6e7bc8660702bb
ben-rieth <benrieth3@gmail.com>
2022-12-13 17:07:39 -0600

    Add link components to drawer items

--- src/components/molecules/Drawer.tsx
+++ src/components/molecules/Drawer.tsx
@@ -1,6 +1,40 @@
 import { type FC } from "react";
 import classNames from "classnames";
 import { AiOutlineClose } from 'react-icons/ai';
+import Link from "next/link";
+import { CATEGORIES } from "../../constants/categories";
+
+type DrawerLinkProps = {
+    title: string;
+    href: string;
+}
+
+const DrawerLink: FC<DrawerLinkProps> = ({ title, href }) => {
+    return (
+        <li className="my-1">
+            <Link href={href}>
+                {title}
+            </Link>
+        </li>
+    );
+};
+
+type DrawerAccordionProps = {
+    title: string;
+    children: JSX.Element | JSX.Element[];
+};
+
+const DrawerAccordion:FC<DrawerAccordionProps> = ({ title, children }) => {
+    
+    return (
+        <li>
+            <p>{title}</p>
+            <ul className="ml-5">
+                {children}
+            </ul>
+        </li>
+    );
+};
 
 type DrawerProps = {
     open: boolean;
@@ -28,19 +62,23 @@ const Drawer:FC<DrawerProps> = ({ open, handleClose }) => {
     )
 
     return (
-        <aside className={drawerClasses}>
+        <aside className={drawerClasses} onClick={handleClose}>
             <AiOutlineClose 
                 className={closeClasses}
                 onClick={handleClose}
             />
-            <ul>
-                <li>Home</li>
-                <li>Latest Posts</li>
-                <ul>
-                    <li>Web Dev</li>
-                    <li>Psychology</li>
-                    <li>Misc</li>
-                </ul>
+            <ul className="mt-20 ml-5 text-4xl">
+                <DrawerLink title="Home" href="/" />
+                <DrawerLink title="Latest Posts" href="/" />
+                <DrawerAccordion title="Categories">
+                    {CATEGORIES.map((category) => (
+                        <DrawerLink 
+                            key={category.id} 
+                            title={category.main} 
+                            href={`/category/${category.id}`}
+                        />
+                    ))}
+                </DrawerAccordion>
             </ul>
         </aside>
     )
