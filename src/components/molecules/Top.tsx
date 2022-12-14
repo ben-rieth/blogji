d32807ffc04786f625e28f8d8b53db45ad3b69c9
ben-rieth <benrieth3@gmail.com>
2022-12-14 17:35:58 -0600

    Add headshot to hero

--- src/components/molecules/Top.tsx
+++ src/components/molecules/Top.tsx
@@ -1,12 +1,26 @@
+import Image from "next/image";
 import SearchBox from "../atoms/SearchBox";
 
 const Top = () => {
     return (
-        <header className="flex justify-between items-center flex-col gap-3 mx-5 mb-20 mt-10">
+        <header className="flex justify-between items-center gap-5 flex-col md:flex-row-reverse md:justify-center md:gap-32 mx-5 mb-20 mt-10">
+            
+            <div>
+                <p className="text-3xl font-mono text-rose-500">
+                    <Image 
+                        src="/me.jpg"
+                        alt="Me, smiling"
+                        width={300}
+                        height={300}
+                        className="rounded-full"
+                    />
+                </p>
+            </div>
+            
             <div className="flex flex-col gap-6">
-                <h1 className="text-3xl text-slate-100 font-handwriting">
+                <p className="text-3xl text-slate-100 font-handwriting">
                     A collection of my latest thoughts on different subjects
-                </h1>
+                </p>
                 <SearchBox />
             </div>
         </header>
