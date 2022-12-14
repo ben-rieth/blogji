dc4c0273ed8ffe52a5c9bf48d59468126d4973e0
ben-rieth <benrieth3@gmail.com>
2022-12-14 17:58:05 -0600

    Add more space to Post Cover

--- src/components/molecules/PostCover.tsx
+++ src/components/molecules/PostCover.tsx
@@ -25,7 +25,7 @@ const PostCover: FC<PostCoverProps> = ({ post }) => {
                 <p className="text-slate-400 mt-2 2xl:text-xl">
                     {formattedDate}&nbsp;&nbsp;•&nbsp;&nbsp;{post.readingTime} {minuteOrMinutes}
                 </p>
-                <h3 className="font-bold text-xl 2xl:text-3xl my-1 group-hover:text-rose-500 leading-snug sm:h-16 2xl:h-24 text-black dark:text-white">
+                <h3 className="font-semibold text-xl 2xl:text-3xl my-1 group-hover:text-rose-500 leading-snug sm:h-24 2xl:h-32 text-black dark:text-white">
                     {post.title}
                 </h3>
                 <button
