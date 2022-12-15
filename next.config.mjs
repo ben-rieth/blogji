1dbde827f84e2200924e57337e0e622b4c34f31a
ben-rieth <benrieth3@gmail.com>
2022-12-15 11:16:24 -0600

    Remove unused imports

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
-import { env } from './src/env/server.mjs';
-
 let ContentSecurityPolicy = `
   default-src 'none';
   base-uri 'self';
