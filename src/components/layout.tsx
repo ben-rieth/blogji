e782653c4cafaf5c4b65ad2aed25bd03fa4e10f9
ben-rieth <benrieth3@gmail.com>
2022-12-17 16:54:02 -0600

    Add footer that stays in the right place

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { type FC } from "react";
 import NavBar from "./navigation/NavBar";
+import Footer from "./structure/Footer";
 
 type LayoutProps = {
     children: JSX.Element[];
@@ -16,10 +17,14 @@ const Layout:FC<LayoutProps> = ({ children, title, description }) => {
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="description" content={description} />
             </Head>
-            <main className="dark:bg-slate-800 min-h-screen">
+            <div className="dark:bg-slate-800 min-h-screen flex flex-col">
                 <NavBar />
-                {children}
-            </main>
+                <main className="flex-1">
+                    {children}
+                </main>
+                <Footer />
+            </div>
+            
         </>
     )
 }
---
+++ src/components/structure/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="flex flex-col justify-center items-center bg-neutral-200 dark:bg-slate-700 w-full">
+            <p className="text-rose-500 text-2xl text-center">
+                Benji Riethmeier
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
