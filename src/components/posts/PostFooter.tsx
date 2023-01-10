0638af7316f08f631e44db8ec54d3a3ead9d43ad
ben-rieth <benrieth3@gmail.com>
2023-01-10 14:58:58 -0600

    Display question mark if views not recieved

--- src/components/posts/PostFooter.tsx
+++ src/components/posts/PostFooter.tsx
@@ -37,10 +37,10 @@ const PostFooter:FC<PostFooterProps> = ({ date, title }) => {
     return (
         <div 
             suppressHydrationWarning
-            className="flex items-center justify-between w-full dark:text-slate-400 text-slate-600 mx-auto max-w-screen-lg px-10 text-lg mb-5"
+            className="flex items-center justify-between w-full dark:text-slate-400 text-slate-600 mx-auto max-w-screen-md px-10 text-base mb-5"
         >
             <p>Published On: {formatMMMMdoYYYY(date)}</p>
-            <p>Views: {views}</p>
+            <p>Views: {views ? views : "?"}</p>
         </div>
     );
 }
